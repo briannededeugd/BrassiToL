569b68d535716ba5cf020eb42ad2bf5684bef7ae
Brianne <92082302+briannededeugd@users.noreply.github.com>
2024-07-09 15:50:14 +0200

    FEAT/FIX: Succesfully add filters to the URL as parameters

--- src/lib/components/queryStore.js
+++ src/lib/components/queryStore.js
@@ -29,6 +29,7 @@ export function createCategoryStore(initialCategories) {
 
       const urlSearchParams = new URLSearchParams(query);
       const g = `?${urlSearchParams.toString()}`;
+      console.log("Navigating to URL:", g);
       goto(g, { keepFocus: true, replaceState: true, noScroll: true });
 
       set(categories);
