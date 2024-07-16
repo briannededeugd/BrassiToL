cb2a283371fe94bd63df37864b0a4e784c44c6b7
Brianne <92082302+briannededeugd@users.noreply.github.com>
2024-07-16 15:53:51 +0200

    ADD: Prettier install and run

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,54 +4,54 @@
 </svelte:head>
 
 <section id="footer">
-	<section id="maincontent">
-		<div id="about">
-			<h3>About</h3>
-			<p>
-				Explore the Brassicaceae family using this interactive phylogenetic tree
-				tool. Dive into the diversity of species, navigate the tree with ease,
-				and uncover the intricate connections within this plant family. This
-				tool is curated by Kasper Hendriks and his botanical research.
-			</p>
-			<p>
-				Hendriks, K. P., Kiefer, C., Al-Shehbaz, I. A., Bailey, C. D., van
-				Huysduynen, A. H., Nikolov, L. A., ... & Lens, F. (2023). Global
-				Brassicaceae phylogeny based on filtering of 1,000-gene dataset. <em
-					>Current Biology</em
-				>, 33(19), 4052-4068.
-			</p>
-		</div>
-		<div id="quicklinks">
-			<h3>Quick Links</h3>
-			<ul>
-				<li>
-					<a
-						href="https://www.linkedin.com/in/kasper-hendriks-b4258711/?originalSubdomain=nl"
-						>Kasper Hendriks</a
-					>
-				</li>
-				<li>
-					<a href="https://powo.science.kew.org/">POWO</a>
-				</li>
-				<li>
-					<a href="https://www.naturalis.nl/en">Naturalis</a>
-				</li>
-				<li>
-					<a href="https://www.inaturalist.org/">iNaturalist</a>
-				</li>
-				<li>
-					<a href="https://www.uni-osnabrueck.de/en/home/"
-						>Osnabrück University</a
-					>
-				</li>
-			</ul>
-		</div>
-	</section>
-	<section id="credits">
-		<p>
-			<span>This website was created by </span> Brianne de Deugd, Robin Dekker and
-			Roosmarijn Hoekstra of the Amsterdam University of Applied Sciences, January
-			2024, in collaboration with Kasper Hendriks of Naturalis Biodiversity Center.
-		</p>
-	</section>
+  <section id="maincontent">
+    <div id="about">
+      <h3>About</h3>
+      <p>
+        Explore the Brassicaceae family using this interactive phylogenetic tree
+        tool. Dive into the diversity of species, navigate the tree with ease,
+        and uncover the intricate connections within this plant family. This
+        tool is curated by Kasper Hendriks and his botanical research.
+      </p>
+      <p>
+        Hendriks, K. P., Kiefer, C., Al-Shehbaz, I. A., Bailey, C. D., van
+        Huysduynen, A. H., Nikolov, L. A., ... & Lens, F. (2023). Global
+        Brassicaceae phylogeny based on filtering of 1,000-gene dataset. <em
+          >Current Biology</em
+        >, 33(19), 4052-4068.
+      </p>
+    </div>
+    <div id="quicklinks">
+      <h3>Quick Links</h3>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/kasper-hendriks-b4258711/?originalSubdomain=nl"
+            >Kasper Hendriks</a
+          >
+        </li>
+        <li>
+          <a href="https://powo.science.kew.org/">POWO</a>
+        </li>
+        <li>
+          <a href="https://www.naturalis.nl/en">Naturalis</a>
+        </li>
+        <li>
+          <a href="https://www.inaturalist.org/">iNaturalist</a>
+        </li>
+        <li>
+          <a href="https://www.uni-osnabrueck.de/en/home/"
+            >Osnabrück University</a
+          >
+        </li>
+      </ul>
+    </div>
+  </section>
+  <section id="credits">
+    <p>
+      <span>This website was created by </span> Brianne de Deugd, Robin Dekker and
+      Roosmarijn Hoekstra of the Amsterdam University of Applied Sciences, January
+      2024, in collaboration with Kasper Hendriks of Naturalis Biodiversity Center.
+    </p>
+  </section>
 </section>
