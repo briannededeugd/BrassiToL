587ba0208c602a5e9d35d50163cf7bece3042824
Brianne <92082302+briannededeugd@users.noreply.github.com>
2024-08-19 13:18:58 +0200

    ADD: All authors

--- static/scripts/csvtojson.js
+++ static/scripts/csvtojson.js
@@ -1,17 +1,19 @@
 import csvtojson from "csvtojson";
 import { promises as fs } from "fs";
 
-const csvFilePath = "BrassiToL_metadata.csv";
-const jsonFilePath = "BrassiToL_metadata.json";
+const csvFilePath =
+  "/Users/briannededeugd/Desktop/BrassiToL/static/csv/BrassiToL_authors.csv";
+const jsonFilePath =
+  "/Users/briannededeugd/Desktop/BrassiToL/static/metadata/BrassiToL__authors_metadata.json";
 
 csvtojson({
-	delimiter: ";", // Have to set the delimiter to a semicolon apparently
+  delimiter: ";", // Have to set the delimiter to a semicolon apparently
 })
-	.fromFile(csvFilePath)
-	.then(async (json) => {
-		await fs.writeFile(jsonFilePath, JSON.stringify(json, null, 4));
-		console.log("Conversion completed, JSON saved to " + jsonFilePath);
-	})
-	.catch((error) => {
-		console.error("An error occurred:", error);
-	});
+  .fromFile(csvFilePath)
+  .then(async (json) => {
+    await fs.writeFile(jsonFilePath, JSON.stringify(json, null, 4));
+    console.log("Conversion completed, JSON saved to " + jsonFilePath);
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
