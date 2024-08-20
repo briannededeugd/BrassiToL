716e5d73d66f3756d6afb645104c5ad6d2bd8c5f
Brianne <92082302+briannededeugd@users.noreply.github.com>
2024-08-20 09:49:16 +0200

    FIX: Remove unnecessary loadData function in SearchBar

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,7 +1,5 @@
 <script>
-  import { onMount } from "svelte";
   import { writable } from "svelte/store";
-  import { Converter } from "csvtojson/v2/Converter";
 
   /**========================================================================
    *                           DECLARING VARIABLES
@@ -10,45 +8,11 @@
   let data = []; // Empty data array so that loaded data is available globally
   let searchQuery = ""; // The user's search term
   const suggestions = writable([]); // Make suggestions based on the search query
-  let converter = new Converter({}); // Coverts CSV to JSON
 
   /**========================================================================
-   *                         ONMOUNT: ON INITIAL PAGE LOAD
-   *
-   *                    Load in all data before the page loads
-   *            Fetch data and make sure it's available at all times
-   *          Populate variables that are needed for the site to work
+   *                FUNCTIONS TO MAKE SUGGESTIONS
    *========================================================================**/
-  onMount(() => {
-    loadData();
-  });
 
-  /**========================================================================
-   *                FUNCTIONS TO LOAD DATA + MAKE SUGGESTIONS
-   *========================================================================**/
-  /**
-   * @name loadData
-   * @role Converts CSV-data to JSON
-   */
-  async function loadData() {
-    try {
-      converter.fromFile("BrassiToL_metadata.csv", function (err, result) {
-        var csvData = JSON.stringify([
-          { resultdata: result[0] },
-          { resultdata: result[1] },
-          { resultdata: result[2] },
-          { resultdata: result[3] },
-          { resultdata: result[4] },
-        ]);
-        csvData = JSON.parse(csvData);
-        console.log(csvData);
-      });
-    } catch (error) {
-      console.error("Error loading the TSV data:", error);
-    }
-  }
-
-  // Function to update suggestions based on search query
   /**
    * @name updateSuggestions
    * @role Load suggestions based on data and the query
