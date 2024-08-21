2bff77bc5e63794c0066dba4ed167229c618189d
Brianne <92082302+briannededeugd@users.noreply.github.com>
2024-08-21 12:05:39 +0200

    FIX: Last fixes

--- static/scripts/csvtojson.js
+++ static/scripts/csvtojson.js
@@ -2,9 +2,9 @@ import csvtojson from "csvtojson";
 import { promises as fs } from "fs";
 
 const csvFilePath =
-  "/Users/briannededeugd/Desktop/BrassiToL/static/csv/BrassiToL_authorsspecimen.csv";
+  "/Users/briannededeugd/Desktop/BrassiToL/static/csv/fixedauthors.csv";
 const jsonFilePath =
-  "/Users/briannededeugd/Desktop/BrassiToL/static/metadata/BrassiToL_authorsspecimen.json";
+  "/Users/briannededeugd/Desktop/BrassiToL/static/metadata/BrassiToL_fixedauthors.json";
 
 csvtojson({
   delimiter: ";", // Have to set the delimiter to a semicolon apparently
